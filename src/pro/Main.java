a5b79c8cecd7f771d164760a1b59f317ff036462
YMS12138 <394321778@qq.com>
2019-11-04 19:42:03 +0800

    一些注释与细节

--- src/pro/Main.java
+++ src/pro/Main.java
@@ -9,8 +9,13 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+/**
+ * 基于 C/S 架构的智能制造协同共享管理平台
+ * 采用JavaFX技术
+ *
+ * @author YMS
+ */
 public class Main extends Application {
 
     private Stage page;
@@ -34,6 +39,12 @@ public class Main extends Application {
         page.show();
     }
 
+    /**
+     * 需求计划创建页面
+     *
+     * @throws IOException
+     * @date 2019/11/4
+     */
     private void requirement() throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("view/requirement.fxml"));
@@ -42,11 +53,21 @@ public class Main extends Application {
         root.setCenter(requirement);
     }
 
+    /**
+     * 年度计划页面(与紧急页面相同)
+     *
+     * @throws IOException
+     * @date 2019/11/4
+     */
     private void showYears() throws IOException {
+        //新建一个加载器
         FXMLLoader loader = new FXMLLoader();
+        //设置加载文件
         loader.setLocation(Main.class.getResource("view/years.fxml"));
+        //加载文件至 info
         AnchorPane info = loader.load();
 
+        //将 info 内嵌进父节点
         GridPane center = (GridPane) root.getCenter();
         center.add(info, 1, 2);
     }
--- src/pro/controller/YearsController.java
+++ src/pro/controller/YearsController.java
@@ -2,10 +2,21 @@ package pro.controller;
 
 import javafx.fxml.FXML;
 
+/**
+ * 需求年度计划页面
+ *
+ * @author YMS
+ */
 public class YearsController {
 
     @FXML
     private void init(){
 
+    }
+
+    @FXML
+    private void save(){
+        //TODO:anything
+        //...
     }
 }
